6516815ef0c80510c720fae2603434957ef1acb0
Federico Galan <fegv.2@hotmail.com.ar>
2023-01-06 15:39:53 -0300

    Se agrega la funcionalidad que valida si el input ingresad es valido o no

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+let encrypt         = document.querySelector('.encrypt-btn');
+let decrypt         = document.querySelector('.decrypt-btn');
+let text            = document.querySelector('.encrypt-input');
+let img             = document.querySelector('.encrypt-result-img');
+let result          = document.querySelector('.encrypt-result-text');
+let resultTitle     = document.querySelector('.encrypt-result-title');
+let resultParagraph = document.querySelector('.encrypt-result-paragraph');
+let copy            = document.querySelector('.copy-btn');
+let adviceIcon      = document.querySelector('.encrypt-comments-icon');
+let adviceText      = document.querySelector('.encrypt-comments-paragraph');
+
+const regExp = /[^a-z\s]/;
+
 const encryptCode = {
     'a' : 'ai',
     'e' : 'enter',
@@ -6,10 +19,35 @@ const encryptCode = {
     'u' : 'ufat'
 }
 
+function isValid(text) {
+
+    if (regExp.test(text)) {
+        return false;
+    } else {
+        return true;
+    }
+    
+}
+
+let getText = () => {
+    
+    let encryptText = text.value;
+
+    if (isValid(encryptText)) {
+        adviceIcon.classList.remove('danger');
+        adviceText.classList.remove('dangerText');
+    } else {
+        adviceIcon.classList.add('danger');
+        adviceText.classList.add('dangerText');
+    }
+
+}
+
+
 let encryptFunction = (text) => {
     
     let newText = '';
-
+    
     for (let i = 0; i < text.length; i++) {
         
         if (text[i] === 'a') {
@@ -31,4 +69,4 @@ let encryptFunction = (text) => {
 }
 
 
-
+text.addEventListener('input', getText);
